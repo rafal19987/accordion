ae54a48f4d2f7722fb166ac38bf4275913d7164c
Rafixx <rafal987.1@o2.pl>
2024-10-26 12:01:17 +0200

    chore: improve project structure

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { poppins } from '../fonts';
+import { poppins } from '@/fonts';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
