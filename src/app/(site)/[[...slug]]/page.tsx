f3328748559841c12ee98b2fe4f04b268844a700
Rafixx <rafal987.1@o2.pl>
2024-10-27 20:01:42 +0100

    refactor: move groq questions to separate file / improve metadata object

--- src/app/(site)/[[...slug]]/page.tsx
+++ src/app/(site)/[[...slug]]/page.tsx
@@ -1,52 +1,19 @@
+import { notFound } from 'next/navigation';
 import { type Metadata } from 'next';
 import { client } from '@/sanity/lib/client';
-import { groq } from 'next-sanity';
-import { notFound } from 'next/navigation';
 import { SECTION_COMPONENTS } from '@/components/Components';
-import { PortableTextQuery } from '@/components/ui/portableText';
+import {
+  getPageBody,
+  getAllPagesSlugs,
+  getPageBySlug,
+  getPageMetadata,
+} from '@/queries';
 
 type Props = { params: { slug?: string[] } };
-
-const getAllPagesSlugs = groq`
-   *[_type == "page"]{
-    "slug": slug.current
-  }[].slug
-`;
-
-const getPageBySlug = groq`
-  *[_type == "page" && slug.current == $slug][0]{
-    ...,
-    components[]{...,
-      _type == "image" => {..., asset->}
-    }
-  }
-`;
-
-const getPageBody = groq`
-*[_type == "page" && slug.current == $slug]{
-    components[] {
-      _type,
-      sectionId,
-      _type == "Faq" => {
-        ${PortableTextQuery('heading')}
-        list[] -> {
-          question,
-          answer
-        }
-      }
-    }
-}[0].components
-`;
 
 const fetchPageBody = async (slug: string) => {
   return await client.fetch(getPageBody, { slug }, { cache: 'no-cache' });
 };
-
-const getPageMetadata = groq`
-  *[_type == "page" && slug.current == $slug]{
-    seo
-  }[0].seo
-`;
 
 const fetchAllPagesSlugs = async () => {
   return await client.fetch(getAllPagesSlugs, {}, { cache: 'no-cache' });
@@ -63,14 +30,28 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   if (!metadata) return {};
 
   return {
+    metadataBase: new URL(process.env.NEXT_PUBLIC_HOST_URL!),
     title: metadata.title,
     description: metadata.description,
     openGraph: {
+      type: 'website',
+      countryName: 'Polska',
+      locale: 'pl_PL',
+      title: metadata.title,
+      description: metadata.description,
+      url: new URL(process.env.NEXT_PUBLIC_HOST_URL!),
       images: metadata.ogImage?.asset.url,
+      siteName: metadata.title,
     },
-    authors: {
-      name: metadata.author,
+    twitter: {
+      title: metadata.title,
+      description: metadata.description,
+      card: 'summary_large_image',
+      images: metadata.ogImage?.asset.url,
+      creator: 'Rafał Izdebski',
     },
+    authors: [{ name: 'Rafał Izdebski', url: 'https://rafalizdebski.pl/' }],
+    creator: 'Rafał Izdebski',
   };
 }
 
