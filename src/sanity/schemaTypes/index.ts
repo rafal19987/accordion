14df547c3c708d0c600eb3c3a780807ad569da4d
Rafixx <rafal987.1@o2.pl>
2024-10-27 20:03:10 +0100

    refactor: delete unused schema from sanity export schemas

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -4,7 +4,6 @@ import Page_Collection from '@sanity/schemaTypes/collections/Page_Collection';
 import Faq_Collection from '@sanity/schemaTypes/collections/Faq_Collection';
 import global from '@sanity/schemaTypes/singleTypes/global';
 import Components from '@sanity/schemaTypes/Components';
-import portableText from '@sanity/schemaTypes/ui/portable-text';
 import seo from '@sanity/schemaTypes/ui/seo';
 import sectionId from '@sanity/schemaTypes/ui/sectionId';
 import Heading from '@sanity/schemaTypes/ui/portable-text/Heading';
@@ -13,7 +12,9 @@ const collectionTypes: SchemaTypeDefinition[] = [
   Page_Collection,
   Faq_Collection,
 ];
+
 const singleTypes: SchemaTypeDefinition[] = [global];
+
 const components: SchemaTypeDefinition[] = [Components];
 const ui: SchemaTypeDefinition[] = [
   seo as unknown as SchemaTypeDefinition,
